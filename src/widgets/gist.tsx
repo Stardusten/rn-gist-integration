9a6a7ca3d465007ac49c76c9c7e7e70f6d9d2a94
stardust <xiangbaobao2003@gmail.com>
2022-10-14 20:14:35 +0800

    Fixed: #2 use `useTracker` instead of events api.

--- src/widgets/gist.tsx
+++ src/widgets/gist.tsx
@@ -1,25 +1,16 @@
 import {
   AppEvents, RemId,
   renderWidget,
-  useAPIEventListener, useGetRemsByIdsReactive,
-  usePlugin,
-  useRunAsync,
+  useTracker,
   WidgetLocation,
 } from '@remnote/plugin-sdk';
 import Gist from 'react-gist';
-import { useState } from 'react';
 
 export const GistWidget = () => {
 
-  const plugin = usePlugin();
-  const [change, setChange] = useState(false);
-
-  const remId = useRunAsync(async () => {
+  const gistId = useTracker(async (plugin) => {
     const context = await plugin.widget.getWidgetContext<WidgetLocation.UnderRemEditor>();
-    return context.remId;
-  }, []);
-
-  const gistId = useRunAsync(async () => {
+    const remId = context.remId;
     const rem = await plugin.rem.findOne(remId);
     if (rem) {
       const remTextInMd = await plugin.richText.toMarkdown(rem.text);
@@ -28,10 +19,7 @@ export const GistWidget = () => {
         return match[1];
     }
     return undefined;
-  }, [remId, change]);
-
-  // rerender when rem changed
-  useAPIEventListener(AppEvents.RemChanged, remId, () => setChange(old => !old));
+  });
 
   return gistId
     ? <Gist id={ gistId }/>
